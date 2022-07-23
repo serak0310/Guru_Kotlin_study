f13d12f468a5ac52a2a5a01734f67c35744b1fe2
serak0310 <serak0310@naver.com>
2022-07-23 21:38:52 +0900

    초기화 버튼

--- GroupApp/app/src/main/java/com/example/groupapp/MainActivity.kt
+++ GroupApp/app/src/main/java/com/example/groupapp/MainActivity.kt
@@ -5,11 +5,40 @@ import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var myHelper: myDBHelper
+    lateinit var edtName: EditText
+    lateinit var edtNumber: EditText
+    lateinit var edtNameResult: EditText
+    lateinit var edtNumberResult: EditText
+    lateinit var btnInit: Button
+    lateinit var btnInsert: Button
+    lateinit var btnSelect: Button
+    lateinit var sqlDB: SQLiteDatabase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        edtName = findViewById<EditText>(R.id.edtName)
+        edtNumber = findViewById<EditText>(R.id.edtNumber)
+        edtNameResult = findViewById<EditText>(R.id.edtNameResult)
+        edtNumberResult = findViewById<EditText>(R.id.edtNumberResult)
+        btnInit = findViewById<Button>(R.id.btnInit)
+        btnInsert = findViewById<Button>(R.id.btnInsert)
+        btnSelect = findViewById<Button>(R.id.btnSelect)
+
+        myHelper = myDBHelper(this)
+
+        btnInit.setOnClickListener{
+            sqlDB = myHelper.writableDatabase
+            myHelper.onUpgrade(sqlDB, 1, 2)
+            sqlDB.close()
+        }
     }
 
     inner class myDBHelper(context: Context) : SQLiteOpenHelper (context, "groupDB", null, 1) {
