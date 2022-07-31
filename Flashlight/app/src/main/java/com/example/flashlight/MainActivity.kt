2365be7dcab2f6805896efa596b7a583d16a7f2d
serak0310 <serak0310@naver.com>
2022-08-01 07:12:13 +0900

    [add] 서비스 구현

--- Flashlight/app/src/main/java/com/example/flashlight/MainActivity.kt
+++ Flashlight/app/src/main/java/com/example/flashlight/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.flashlight
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Switch
@@ -13,12 +14,17 @@ class MainActivity : AppCompatActivity() {
 
         flashSwitch = findViewById(R.id.FlashSwitch)
         val torch = Torch(this)
+        val intent = Intent(this, TorchService::class.java)
 
         flashSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
             if(isChecked){
-                torch.flashOn()
+                //torch.flashOn()
+                intent.action ="on"
+                startService(intent)
             }else{
-                torch.flashOff()
+                //torch.flashOff()
+                intent.action ="off"
+                startService(intent)
             }
         }
     }
--- Flashlight/app/src/main/java/com/example/flashlight/TorchService.kt
+++ Flashlight/app/src/main/java/com/example/flashlight/TorchService.kt
@@ -6,6 +6,23 @@ import android.os.IBinder
 
 class TorchService : Service() {
 
+    private val torch: Torch by lazy {
+        Torch(this)
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
+        when(intent?.action){
+            "on" -> {
+                torch.flashOn()
+            }
+            "off" -> {
+                torch.flashOff()
+            }
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
+
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
     }
